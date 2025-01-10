48529f4ba751fa132c4b7c1b4c67e4d370ad5a61
David.Ellis <david.ellis@birmingham.gov.uk>
2025-01-10 16:09:13 +0000

    Converted coordinates from OSGB to long and lat

--- R/functions.R
+++ R/functions.R
@@ -3,7 +3,24 @@ source("R/config.R")
 library(dplyr)
 library(sf)
 
+# Convert British National Grid coordinates
+OSGB_convert <- function(
+    df
+) {
+  # Convert to an sf object, specifying the EPSG code for OSGB36 (EPSG:27700)
+  osgb_coords <- st_as_sf(df, coords = c("Easting", "Northing"), crs = 27700)
+  
+  # Transform to WGS84 (EPSG:4326 for latitude and longitude)
+  wgs_coords <- st_transform(osgb_coords, crs = 4326)
+  
+  # Store the results
+  lat_and_long <- st_coordinates(wgs_coords)
+  df$LONG <- lat_and_long[,"X"]
+  df$LAT <- lat_and_long[,"Y"]
+  return(df)
+}
 
+# Download diffuser tube data from http://62.65.40.208/
 get_dtube_data <- function(
     redownload = FALSE
 ) {
@@ -17,8 +34,8 @@ get_dtube_data <- function(
     diffusion_url_prefix <- "http://62.65.40.208/cgi-bin/birmingham/apub.exportDiffTube.cgi/graph.txt?outtype=txt&from="
     download_path <- paste0("C:/Users/",usr_name,"/Downloads/graph.txt")
     
-    column_names <- c("Year", "Tube_ID", "Address", "num1", 
-                      "num2", "Area_Type", "Recorded_Value")
+    column_names <- c("Year", "Tube_ID", "Address", "Easting", 
+                      "Northing", "Area_Type", "Recorded_Value")
     
     # Download all the files
     data_list <- list()
@@ -39,6 +56,11 @@ get_dtube_data <- function(
     # Combine and save
     diffusion_data <- data.table::rbindlist(data_list) %>%
       arrange(Tube_ID)
+    
+    # Convert coordinates to long and lat
+    diffusion_data <- OSGB_convert(diffusion_data)
+    
+    # Save output
     write.csv(diffusion_data, data_path, row.names = FALSE)
     print("Done.")
   } else{
